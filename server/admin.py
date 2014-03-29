11a88e7bdb98f0b4b90d6f97c390142226bde0b5
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2014-03-29 13:41:42 -0700

    Adding new tables to admin

--- server/admin.py
+++ server/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from server.models import *
+
+admin.site.register(User)
+admin.site.register(Employer)
+admin.site.register(Survey)

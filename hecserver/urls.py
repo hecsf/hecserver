6fb3d815c4ee4e9fd34021f8fd1c6a8536cffc49
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2014-03-29 14:33:48 -0700

    Adding a first view to the app

--- hecserver/urls.py
+++ hecserver/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'hecserver.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^server/', include('server.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )

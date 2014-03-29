435beb7816e1261e597d894566d8680467427210
Allen Pan <allenpan@pocketgems.com>
2014-03-29 15:31:45 -0700

    Added endpoint for sending an sms to AP

--- server/urls.py
+++ server/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from server import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'send_sms', views.send_sms, name='send_sms')
 )
--- server/views.py
+++ server/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from twilio.rest import TwilioRestClient
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
+
+def send_sms(request):
+    account_sid = "AC82d60753961205b203960366b473fdab"
+    auth_token  = "c9936326a39d39213509a0b54efcdaca"
+    client = TwilioRestClient(account_sid, auth_token)
+    message = client.messages.create(body="www.google.com/",
+                                 to="+16263536082",
+                                 from_="+14157809109")
+    return HttpResponse(""+message.sid)

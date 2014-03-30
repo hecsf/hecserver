85d77625fec81fb06b4d0ba531d3dfe2fdb5b1da
Allen Pan <allenpan@pocketgems.com>
2014-03-29 19:58:22 -0700

    Updated survey gizmo test script with reading of 'sguid' from response dict

--- scripts/survey_gizmo_test.py
+++ scripts/survey_gizmo_test.py
@@ -2,12 +2,19 @@
 from surveygizmo import SurveyGizmo
 import json
 
+# authentication
 sg = SurveyGizmo(api_version='v3', response_type='json')
 sg.config.auth_method = "user:pass"
 sg.config.username = "kgruneisen@ecs-sf.org"
 sg.config.password = "HEC123"
 
-response_json = sg.api.surveyresponse.list('1599451')
+# generate a URL with a unique sguid tied to the user
+
+
+# get response for a dict
+survey_id = "1599451"
+response_json = sg.api.surveyresponse.list(survey_id)
 response_dict = json.loads(response_json)
 for user_response in response_dict["data"]:
-    print user_response
+    if "[url(\"sguid\")]" in user_response.keys():
+        print user_response["[url(\"sguid\")]"]
